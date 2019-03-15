68f094efeb9b13d34d28940990409ace730e5312
Stefan Kroboth <stefan.kroboth@gmail.com>
2019-03-15 17:04:35 +0100

    work on scheduler

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ use failure::Error;
 use lazy_static::lazy_static;
 use slog::{info, o, Drain};
 use std::fs::{create_dir, read_dir, rename};
-use std::path::Path;
+use std::path::{Path, PathBuf};
 
 const INBOX: &'static str = "inbox/";
 const OUTBOX: &'static str = "outbox/";
@@ -75,6 +75,147 @@ fn is_empty(path: &Path) -> Result<bool, Error> {
     Ok(true)
 }
 
+struct Scheduler {
+    inbox: PathBuf,
+    queued: PathBuf,
+    outbox: PathBuf,
+    q: Queue,
+}
+
+impl Scheduler {
+    pub fn new() -> Scheduler {
+        Scheduler {
+            inbox: PathBuf::from("inbox"),
+            queued: PathBuf::from("queued"),
+            outbox: PathBuf::from("outbox"),
+            q: Queue::new(),
+        }
+    }
+
+    pub fn inbox(mut self, dir: &str) -> Self {
+        self.inbox = PathBuf::from(dir);
+        self
+    }
+
+    pub fn queued(mut self, dir: &str) -> Self {
+        self.queued = PathBuf::from(dir);
+        self
+    }
+
+    pub fn outbox(mut self, dir: &str) -> Self {
+        self.queued = PathBuf::from(dir);
+        self
+    }
+
+    pub fn run(mut self) -> Result<(), Error> {
+        info!(LOG, "Your choices for the directories were: ");
+        info!(LOG, ""; "inbox" => INBOX, "queued" => QUEUED, "outbox" => OUTBOX);
+
+        self.make_dirs()?;
+        self.collect_leftovers()?;
+
+        loop {
+            self.check_dirs()?;
+            let new_in = self.hit_refresh()?;
+            let new_sched = self.schedule()?;
+
+            if new_in || new_sched {
+                let len = self.q.len();
+                if len == 0 {
+                    info!(LOG, "Queue empty! Hooray!");
+                } else {
+                    info!(LOG, "Current length of queue: {}", len);
+                }
+            }
+
+            std::thread::sleep(std::time::Duration::from_millis(500));
+
+            break;
+        }
+
+        Ok(())
+    }
+
+    fn make_dirs(&mut self) -> Result<(), Error> {
+        if !self.inbox.is_dir() {
+            create_dir(&self.inbox)?;
+        }
+        if !self.queued.is_dir() {
+            create_dir(&self.queued)?;
+        }
+        if !self.outbox.is_dir() {
+            create_dir(&self.outbox)?;
+        }
+        Ok(())
+    }
+
+    fn collect_leftovers(&mut self) -> Result<(), Error> {
+        for leftover in read_dir(&self.queued)? {
+            let leftover = leftover?;
+            let path = leftover.path();
+            if path.is_file() {
+                let filename = path.file_name().unwrap().to_str().unwrap();
+                info!(LOG, "Found leftover: {}", filename);
+                self.q.push(Sequence::new(filename.to_string()));
+            }
+        }
+        Ok(())
+    }
+
+    fn check_dirs(&mut self) -> Result<(), Error> {
+        if check_and_create_dir(&self.inbox)? {
+            info!(
+                LOG,
+                "I had to create the inbox folder again! Whatever wasn't queued already is lost :("
+            );
+        }
+        if check_and_create_dir(&self.queued)? {
+            info!(LOG, "I had to create the queued folder again! This means that the queue is also lost :(");
+            // flush queue because if queued does not exist, queue must be empty!
+            self.q.dump();
+        }
+        if check_and_create_dir(&self.outbox)? {
+            info!(
+                LOG,
+                "I had to create the outbox folder again! I don't know what happend to your job :("
+            );
+        }
+        Ok(())
+    }
+
+    fn hit_refresh(&mut self) -> Result<bool, Error> {
+        let mut changes = false;
+        // get new files
+        for file in read_dir(&self.inbox)? {
+            let file = file?;
+            let path = file.path();
+            if path.is_file() {
+                let filename = path.file_name().unwrap().to_str().unwrap();
+                info!(LOG, "New sequence: {}", filename);
+                rename(&path, &self.queued.join(&filename))?;
+                self.q.push(Sequence::new(filename.to_string()));
+                changes = true;
+            }
+        }
+        Ok(changes)
+    }
+
+    fn schedule(&mut self) -> Result<bool, Error> {
+        let mut changes = false;
+        if is_empty(&self.outbox)? {
+            if let Some(seq) = self.q.pop() {
+                rename(
+                    &self.queued.join(&seq.name),
+                    &self.outbox.join("external.txt"),
+                )?;
+                info!(LOG, "Scheduling: {}", seq.name);
+                changes = true;
+            }
+        }
+        Ok(changes)
+    }
+}
+
 fn check_and_create_dir(dir: &Path) -> Result<bool, Error> {
     if !dir.is_dir() {
         info!(
@@ -88,17 +229,10 @@ fn check_and_create_dir(dir: &Path) -> Result<bool, Error> {
 }
 
 fn run() -> Result<(), Error> {
-    // let decorator = slog_term::TermDecorator::new().build();
-    // let drain = slog_term::FullFormat::new(decorator).build().fuse();
-    // let drain = slog_async::Async::new(drain).build().fuse();
-    // let log: () = slog::Logger::root(drain, o!());
-
     info!(LOG, "------------------------------------------------");
     info!(LOG, "| Welcome!                                     |");
     info!(LOG, "| I'm funnel, a likely error-prone bottleneck! |");
     info!(LOG, "------------------------------------------------");
-    info!(LOG, "Your choices for the directories were: ");
-    info!(LOG, ""; "inbox" => INBOX, "queued" => QUEUED, "outbox" => OUTBOX);
 
     let inbox = Path::new(INBOX);
     let outbox = Path::new(OUTBOX);
